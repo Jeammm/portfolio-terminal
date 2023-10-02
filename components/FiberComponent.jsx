cfe5fb7a3ee50029dabb8a35aae41cd7f0a0d9b5
Jeammm <87964996+Jeammm@users.noreply.github.com>
2023-10-02 23:28:50 +0700

    lol

--- components/FiberComponent.jsx
+++ components/FiberComponent.jsx
@@ -4,21 +4,28 @@ import { Canvas, useFrame } from "@react-three/fiber";
 function Box(props) {
   const meshRef = useRef();
   const [hovered, setHover] = useState(false);
+  const [r, setR] = useState(50);
+  const [g, setG] = useState(50);
+  const [b, setB] = useState(50);
+
   useFrame((state, delta) => {
     meshRef.current.rotation.x += delta;
     meshRef.current.rotation.y += delta;
+    setR((p) => (p + 3)%255);
+    setG((p) => (p + 5)%255);
+    setB((p) => (p + 7)%255);
   });
 
   useEffect(() => {
-    const handleScroll = event => {
+    const handleScroll = (event) => {
       meshRef.current.rotation.x += 0.05;
       meshRef.current.rotation.y += 0.05;
     };
-  
-    window.addEventListener('scroll', handleScroll);
-  
+
+    window.addEventListener("scroll", handleScroll);
+
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -30,7 +37,7 @@ function Box(props) {
       onPointerOut={(event) => setHover(false)}
     >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "red" : "lime"} />
+      <meshStandardMaterial color={hovered ? "red" : `rgb(${r},${g},${b})`} />
     </mesh>
   );
 }
@@ -38,8 +45,8 @@ function Box(props) {
 export default function FiberComponent() {
   return (
     <div className="absolute">
-      <Canvas >
-        {/* <ambientLight /> */}
+      <Canvas>
+        <ambientLight />
         <pointLight position={[-3.4, 1.5, 1]} />
         <Box position={[-4.5, 2, 0]} />
       </Canvas>
