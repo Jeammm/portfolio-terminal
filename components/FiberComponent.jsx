efda92a437b044677d92b38ac713a6b59000cadd
Jeammm <87964996+Jeammm@users.noreply.github.com>
2025-04-28 22:06:01 +0700

    box to heart

--- components/FiberComponent.jsx
+++ components/FiberComponent.jsx
@@ -1,32 +1,46 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useMemo } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
+import { Shape } from "three";
 
-function Box(props) {
+function Heart(props) {
   const meshRef = useRef();
   const [hovered, setHover] = useState(false);
   const [r, setR] = useState(1);
   const [g, setG] = useState(125);
   const [b, setB] = useState(200);
 
+  // Heart shape
+  const heartShape = useMemo(() => {
+    const x = 0,
+      y = 0;
+    const shape = new Shape();
+    shape.moveTo(x + 5, y + 5);
+    shape.bezierCurveTo(x + 5, y + 5, x + 4, y, x, y);
+    shape.bezierCurveTo(x - 6, y, x - 6, y + 7, x - 6, y + 7);
+    shape.bezierCurveTo(x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19);
+    shape.bezierCurveTo(x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7);
+    shape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y);
+    shape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5);
+    return shape;
+  }, []);
+
   useFrame((state, delta) => {
-    meshRef.current.rotation.x += delta;
-    meshRef.current.rotation.y += delta;
-    setR((p) => (p + 3)%255);
-    setG((p) => (p + 5)%255);
-    setB((p) => (p + 7)%255);
+    if (meshRef.current) {
+      meshRef.current.rotation.y += delta * 2;
+    }
+    setR((p) => ((p + 3) % 125) + 125);
+    setG((p) => ((p + 5) % 125) + 125);
+    setB((p) => ((p + 7) % 125) + 125);
   });
 
   useEffect(() => {
-    const handleScroll = (event) => {
-      meshRef.current.rotation.x += 0.05;
-      meshRef.current.rotation.y += 0.05;
+    const handleScroll = () => {
+      if (meshRef.current) {
+        meshRef.current.rotation.y += 0.05;
+      }
     };
-
     window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
@@ -35,8 +49,26 @@ function Box(props) {
       ref={meshRef}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
+      scale={0.06}
+      rotation={[Math.PI, 0, 0]}
     >
-      <boxGeometry args={[1, 1, 1]} />
+      <extrudeGeometry
+        args={[
+          heartShape,
+          {
+            depth: 5,
+            bevelEnabled: true,
+            bevelThickness: 5,
+            bevelSize: 5,
+            bevelSegments: 10,
+          },
+        ]}
+        ref={(geo) => {
+          if (geo) {
+            geo.center();
+          }
+        }}
+      />
       <meshStandardMaterial color={hovered ? "red" : `rgb(${r},${g},${b})`} />
     </mesh>
   );
@@ -48,7 +80,7 @@ export default function FiberComponent() {
       <Canvas>
         <ambientLight />
         <pointLight position={[-3.4, 1.5, 1]} />
-        <Box position={[-4.5, 2, 0]} />
+        <Heart position={[-4.5, 2, 0]} />
       </Canvas>
     </div>
   );
